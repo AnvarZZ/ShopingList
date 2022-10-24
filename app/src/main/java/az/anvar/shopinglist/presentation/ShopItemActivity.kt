ae9ed69fb2dc2f7ffcf63d3172f15ac356dc98c5
AnvarZZ <anvarzeynalzade@gmail.com>
2022-10-24 11:57:33 +0400

    ShopItemActivity (2nd screen) was created

---
+++ app/src/main/java/az/anvar/shopinglist/presentation/ShopItemActivity.kt
@@ -0,0 +1,12 @@
+package az.anvar.shopinglist.presentation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import az.anvar.shopinglist.R
+
+class ShopItemActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_shop_item)
+    }
+}
